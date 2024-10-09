78f992f1704406b10a14485b179319448ee7491a
Lucas Pita Lima <lucaspitalima@gmail.com>
2024-10-09 00:54:55 -0300

    refactor

--- Controllers/TarefaController.cs
+++ Controllers/TarefaController.cs
@@ -92,10 +92,13 @@ namespace TrilhaApiDesafio.Controllers
 
             tarefaBanco.Titulo = tarefa.Titulo;
             tarefaBanco.Descricao = tarefa.Descricao;
-            tarefaBanco.Data = tarefa.Data; 
+            tarefaBanco.Data = tarefa.Data;
+            tarefaBanco.Status = tarefa.Status;
+
+            _context.SaveChanges();
             // TODO: Atualizar as informações da variável tarefaBanco com a tarefa recebida via parâmetro
             // TODO: Atualizar a variável tarefaBanco no EF e salvar as mudanças (save changes)
-            return Ok();
+            return Ok(tarefaBanco);
         }
 
         [HttpDelete("{id}")]
@@ -106,6 +109,8 @@ namespace TrilhaApiDesafio.Controllers
             if (tarefaBanco == null)
                 return NotFound();
 
+            _context.Tarefas.Remove(tarefaBanco);
+            _context.SaveChanges();
             // TODO: Remover a tarefa encontrada através do EF e salvar as mudanças (save changes)
             return NoContent();
         }
